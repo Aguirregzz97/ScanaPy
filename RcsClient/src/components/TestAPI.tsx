90fb9307f91c84d050b5bf412ffafe74c39726eb
Andres <aguirregzz97@gmail.com>
2018-08-25 23:24:22 -0500

    working with almost everything ye boi

--- RcsClient/src/components/TestAPI.tsx
+++ RcsClient/src/components/TestAPI.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react'
+import { SpringGrid, makeResponsive } from 'react-stonecutter'
+
+const Grid = makeResponsive(SpringGrid, { maxWidth: 1920 })
+
+const computer = require('./../assets/img/computer.png')
+
 
 type ApiState = {
     name: string
@@ -6,9 +12,11 @@ type ApiState = {
 
 type State = {
     dataFromAPI: ApiState
+    arrayComputer: any[]
+    indexSelected: number
 }
 
-type Props  = {
+type Props = {
 }
 
 export default class TestAPI extends React.Component<Props, State> {
@@ -16,31 +24,105 @@ export default class TestAPI extends React.Component<Props, State> {
     constructor(props) {
         super(props)
         this.state = {
-            dataFromAPI: null
+            dataFromAPI: null,
+            indexSelected: null,
+            arrayComputer: [
+                {
+                    ip: '1',
+                    brand: 'acer'
+                },
+                {
+                    ip: '2',
+                    brand: 'apple'
+                },
+                {
+                    ip: '3',
+                    brand: 'razr'
+                },
+                {
+                    ip: '4',
+                    brand: 'asus'
+                },
+                {
+                    ip: '5',
+                    brand: 'gigabyte'
+                },
+                {
+                    ip: '6',
+                    brand: 'dell'
+                },
+                {
+                    ip: '7',
+                    brand: 'google'
+                },
+                {
+                    ip: '8',
+                    brand: 'HP'
+                },
+            ],
         }
     }
 
     componentDidMount() {
-        fetch('http://192.168.1.132:5000')
-        .then(d => d.json())
-        .then(d => {
-            this.setState({
-                dataFromAPI: d
+        fetch('http://192.168.1.138:000')
+            .then(d => d.json())
+            .then(d => {
+                this.setState({
+                    dataFromAPI: d
+                })
             })
-            console.log(this.state.dataFromAPI)
+    }
+
+    selectComputer(indexSel) {
+        this.setState({
+            indexSelected: indexSel
         })
     }
 
     render() {
         if (!this.state.dataFromAPI) return (
             <div>
-                <h1>loading...</h1>
+                <div style={{ marginBottom: '20px' }} ></div>
+                <Grid
+                    component='ul'
+                    columns={5}
+                    columnWidth={250}
+                    gutterWidth={5}
+                    gutterHeight={20}
+                    itemHeight={200}
+                    springConfig={{ stiffness: 170, damping: 26 }}
+                >
+                    {this.state.arrayComputer.map((num, index) => {
+                        return <a href='' onClick={this.selectComputer.bind(this, index)} data-toggle='modal' data-target='#exampleModal'><img className='computerImage' src={computer} key={index} /></a>
+                    })}
+                </Grid>
+                <div className='modal fade' id='exampleModal' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+                    <div className='modal-dialog' role='document'>
+                        <div className='modal-content'>
+                            <div className='modal-header'>
+                                <h5 className='modal-title' id='exampleModalLabel'>PC # {this.state.indexSelected + 1}</h5>
+                                <button type='button' className='close' data-dismiss='modal' aria-label='Close'>
+                                    <span aria-hidden='true'>&times;</span>
+                                </button>
+                            </div>
+                            <div className='modal-body'>
+                            ip: {this.state.indexSelected && this.state.arrayComputer[this.state.indexSelected].ip}
+                            <br/>
+                            brand: {this.state.indexSelected && this.state.arrayComputer[this.state.indexSelected].brand}
+                            </div>
+                            <div className='modal-footer'>
+                                <button type='button' className='btn btn-secondary' data-dismiss='modal'>Close</button>
+                                <button type='button' className='btn btn-primary'>Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
             </div>
         )
         return (
             <div>
-                <h1>This is test</h1>
-                <h1>{this.state.dataFromAPI.name}</h1>
             </div>
         )
     }
